ee58d0441b51f317160ddb88c9f1214c9097e4d0
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-10-02 20:21:45 +0000

    working with user details

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,16 @@
-class UserController < AplicationController
+class UserController < ApplicationController
   def home
     render({:template => "user_page/home"})
+  end
+
+  def all_users
+    render({:template => "user_page/all_users"})
+  end
+
+  def username
+    name = params.fetch("user_name")
+    @name = User.where({:username => name})[0]
+    render({:template => "user_page/user_details"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get("/", {:controller => "user", :action => "home"})
+  get("/users", {:controller => "user", :action => "all_users"})
+  get("/users/user_name", {:controller => "user", :action => "username"})
 
 end
