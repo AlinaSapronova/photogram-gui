3ffc5c35b73c3c04c950c4ac349c423e1b46b060
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-10-03 02:40:50 +0000

    add new photo is done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,21 @@ class PhotosController < ApplicationController
     @picture = Photo.where({:id => picture_id})[0]
     render({:template => "photos_page/photo_detail"})
   end
+
+  def add_new_photo
+   
+    new_photo = Photo.new
+    new_photo.image = params.fetch("input_image")
+    new_photo.caption = params.fetch("input_caption")
+    new_photo.owner_id = params.fetch("input_owner_id")
+
+    if new_photo.valid?
+      new_photo.save
+      redirect_to("/photos/#{new_photo.id}", { :notice => "Photo posted successfully." })
+    else
+      redirect_to("/photos", { :notice => "Photo failed to post successfully." })
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   get("/users/:username", {:controller => "user", :action => "username"})
   post("/add_user", {:controller => "user", :action => "add_new_user"})
   post("/update_user/:user_id", :controller => "user", :action => "update_user")
+  
 
 
   get("/photos", {:controller => "photos", :action => "all_photos"})
   get("/photos/:photo_id", {:controller => "photos", :action => "photo_detail"})
+  post("/insert_photo_record", {:controller => "photos", :action => "add_new_photo"})
 
 end
